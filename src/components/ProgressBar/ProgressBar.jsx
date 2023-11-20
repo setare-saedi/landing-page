e822c39a527f4fa480f5fe691e17642e7b0df3bb
setare <ssaedi1995@gmail.con>
2023-11-21 00:25:38 +0330

    add progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -4,7 +4,7 @@ import ProgressBar from "@ramonak/react-progress-bar";
 function ProgressBar1({com,}) {
     return (
         <div>
-            <ProgressBar completed={com} maxCompleted={100} animateOnRender={true} height='15px' borderRadius='0' isLabelVisible={true} labelColor={'black'} bgColor={'blueviolet'} transitionDuration={'3s'}/>
+            <ProgressBar completed={com} maxCompleted={100} animateOnRender={true} height='20px' borderRadius='25px' isLabelVisible={true} labelColor={'white'} bgColor={'blueviolet'} transitionDuration={'3s'}/>
         </div>
     );
 }
